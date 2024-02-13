9dca0f36e395ef1c7574d916a6b9f388ab339e4f
Mag Millen-Dutka <145022728+MagMillen-Dutka@users.noreply.github.com>
2024-02-13 14:08:49 +0000

    upload of JS files in models. Added schema information.

---
+++ models/thought.js
@@ -0,0 +1,38 @@
+const { Schema, model } = require("mongoose");
+const dateFormat = require("../utils/dateFormat");
+const reactionsSchema = require("./Reactions");
+
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: "Leave a thought!",
+      minlength: 1,
+      maxlength: 230,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (timestamp) => dateFormat(timestamp),
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionsSchema],
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+thoughtSchema.virtual("reactionsCount").get(function () {
+  return this.reactions.length;
+});
+
+const thought = model("Thought", thoughtSchema);
+
+module.exports = thought;
